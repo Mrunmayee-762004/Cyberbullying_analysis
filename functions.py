1a6790e85bf3e75fa6918798ea1d1919ef2046ab
Mrunmayee-762004 <mrunmayeephadke762004@gmail.com>
2023-10-19 01:59:46 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,210 @@
+# importing libraries
+import pandas as pd
+import numpy as np
+import re
+import string
+import os
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from nltk.tokenize import RegexpTokenizer
+from nltk import PorterStemmer, WordNetLemmatizer
+import pickle
+import nltk
+nltk.download('wordnet')
+
+# preprocessing functions
+
+# converting tweet text to lower case
+def text_lower(text):
+    return text.str.lower()
+
+# removing stopwoords from the tweet text
+def clean_stopwords(text):
+    # stopwords list that needs to be excluded from the data
+    stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+    STOPWORDS = set(stopwordlist)
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+
+# cleaning and removing punctuations
+def clean_puctuations(text):
+    english_puctuations = string.punctuation
+    translator = str.maketrans('','', english_puctuations)
+    return text.translate(translator)
+
+# cleaning and removing repeating characters
+def clean_repeating_characters(text):
+    return re.sub(r'(.)1+', r'1', text)
+
+# cleaning and removing URLs
+def clean_URLs(text):
+    return re.sub(r"((www.[^s]+)|(http\S+))","",text)
+
+# cleaning and removing numeric data
+def clean_numeric(text):
+    return re.sub('[0-9]+', '', text)
+
+# Tokenization of tweet text
+def tokenize_tweet(text):
+    tokenizer = RegexpTokenizer('\w+')
+    text = text.apply(tokenizer.tokenize)
+    return text
+
+# stemming    
+def text_stemming(text):
+    st = PorterStemmer()
+    text = [st.stem(word) for word in text]
+    return text
+
+# lemmatization
+def text_lemmatization(text):
+    lm = WordNetLemmatizer()
+    text = [lm.lemmatize(word) for word in text]
+    return text
+
+# defining preprocess function
+
+def preprocess(text):
+    text = text_lower(text)
+    text = text.apply(lambda text: clean_stopwords(text))
+    text = text.apply(lambda x : clean_puctuations(x))
+    text = text.apply(lambda x: clean_repeating_characters(x))
+    text = text.apply(lambda x : clean_URLs(x))
+    text = text.apply(lambda x: clean_numeric(x))
+    text = tokenize_tweet(text)
+    text = text.apply(lambda x: text_stemming(x))
+    text = text.apply(lambda x: text_lemmatization(x))
+    text = text.apply(lambda x : " ".join(x))
+    return text
+
+# def custom_input_prediction(text):
+#     import nltk
+#     nltk.download('omw-1.4')
+#     text = pd.Series(text)
+#     text = preprocess(text)
+#     text = [text[0],]
+#     # to use this function we will need to define vectoriser first 
+#     vectoriser = pickle.load(open("tdf_vectorizer", "rb"))
+#     text = vectoriser.transform(text)
+#     model = pickle.load(open("model.bin", "rb"))
+#     prediction = model.predict(text)
+#     prediction = prediction[0]
+
+#     interpretations = {
+#         0 : "Bullying",
+#         1 : "Non-Bullying",
+#         2 : "Age",
+#         3 : "Ethnicity",
+#         4 : "Gender",
+#         5 : "Not Cyberbullying",
+#         6 : "Other Cyberbullying",
+#         7 : "Religion"
+#     }
+
+#     for i in interpretations.keys():
+#         if i == prediction:
+#             return interpretations[i]
+        
+# csv
+def custom_input_prediction(text):
+    import nltk
+    nltk.download('omw-1.4')
+    text = pd.Series(text)
+    text = preprocess(text)
+    text = [text[0],]
+    # to use this function we will need to define vectoriser first 
+    vectoriser = pickle.load(open("tdf_vectorizer", "rb"))
+    text = vectoriser.transform(text)
+    model = pickle.load(open("model.bin", "rb"))
+    prediction = model.predict(text)
+    prediction = prediction[0]
+
+    interpretations = {
+        0 : "Bullying",
+        1 : "Non-Bullying",
+        2 : "Age",
+        3 : "Ethnicity",
+        4 : "Gender",
+        5 : "Not Cyberbullying",
+        6 : "Other Cyberbullying",
+        7 : "Religion"
+    }
+    try:
+        data = pd.read_csv('predictions.csv')
+    except FileNotFoundError:
+        data = pd.DataFrame(columns=['Prediction'])
+
+    for i in interpretations.keys():
+        if i == prediction:
+            result = interpretations[i]
+            
+            # Append the result to a CSV file
+            data = pd.DataFrame({'Prediction': [result]})
+            data.to_csv('C:/Users/mrunm/Downloads/ML_CP_TY/predictions.csv', mode='a', header=not data.empty, index=False)
+            # data.to_csv('predictions.csv', mode='a', header=not data.empty, index=False)
+            return result
+
+# def custom_input_prediction(text):
+#     import nltk
+#     nltk.download('omw-1.4')
+#     text = pd.Series(text)
+#     text = preprocess(text)
+#     text = [text[0],]
+
+#     # Load the vectorizer and model
+#     vectorizer = pickle.load(open("tdf_vectorizer", "rb"))
+#     model = pickle.load(open("model.bin", "rb"))
+
+#     # Make a prediction
+#     text_vectorized = vectorizer.transform(text)
+#     prediction = model.predict(text_vectorized)[0]
+
+#     interpretations = {
+#         0: "Bullying",
+#         1: "Non-Bullying",
+#         2: "Age",
+#         3: "Ethnicity",
+#         4: "Gender",
+#         5: "Not Cyberbullying",
+#         6: "Other Cyberbullying",
+#         7: "Religion"
+#     }
+
+#     # Track predictions and count occurrences
+#     predictions_count = {}
+#     if "predictions_count.pkl" in os.listdir():  # Check if the file exists
+#         with open("predictions_count.pkl", "rb") as file:
+#             predictions_count = pickle.load(file)
+
+#     # Update the count for the current prediction
+#     if prediction in predictions_count:
+#         predictions_count[prediction] += 1
+#     else:
+#         predictions_count[prediction] = 1
+
+#     # Save the updated predictions count
+#     with open("predictions_count.pkl", "wb") as file:
+#         pickle.dump(predictions_count, file)
+
+#     # Find the category with maximum cyberbullying occurrences
+#     max_category = max(predictions_count, key=predictions_count.get)
+#     max_count = predictions_count[max_category]
+
+#     return interpretations[prediction], max_category, max_count
